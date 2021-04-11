c9086f4308feec4238d8ffb49a037f1c7fc3791c
AlanNggg <ngpuilam9166@gmail.com>
2021-04-11 22:41:30 +0800

    update category controller: flatten json

--- controllers/productCategoryController.js
+++ controllers/productCategoryController.js
@@ -1,6 +1,7 @@
 const ProductCategory = require("../models/productCategoryModel");
 const catchError = require("../lib/catchError");
 const controllerFactory = require("./controllerFactory");
+const API = require("../lib/API");
 
 exports.deleteCategoryByName = catchError(async (req, res, next) => {
     await ProductCategory.findOneAndDelete({ category: req.params.name });
@@ -13,7 +14,19 @@ exports.deleteCategoryByName = catchError(async (req, res, next) => {
     });
 });
 
-exports.getAllCategories = controllerFactory.getAll(ProductCategory);
+exports.getAllCategories = catchError(async (req, res, next) => {
+    const api = new API(ProductCategory.find(), req.query)
+        .filter()
+        .sort()
+        .select()
+        .paginate();
+
+    const data = await api.query;
+
+    res.status(200).json({
+        data,
+    });
+});
 
 exports.createCategory = controllerFactory.createOne(ProductCategory);
 
